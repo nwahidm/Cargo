b8fa0b41b73e6f6161b965a5814dabb5dd6b34a0
Nur Wahid Muhammad <nwahidm@gmail.com>
2024-02-06 15:51:04 +0700

    add ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,45 +1,45 @@
 let watch = false;
 let instances = 1;
-let exec_mode = 'fork';
+let exec_mode = "fork";
 
 module.exports = {
-    apps: [
-        {
-            name: 'testing',
-            cwd: '/home/clouduser/backend/Testing',
-            script: 'npm start',
-            exp_backoff_restart_delay: 100,
-            instances,
-            exec_mode,
-            watch,
-            max_memory_restart: '1G',
-            autorestart: true,
-            env: {
-                NAMESPACE: 'testing',
-                NODE_ENV: 'development',
-                PORT: 3000,
-                SECRET: 'SECRET',
-                DB_CONNECTION: 'postgres',
-                DB_HOST: '202.157.188.102',
-                DB_PORT: 5432,
-                DB_NAME: 'Testing',
-                DB_USERNAME: 'w1_user',
-                DB_PASSWORD: 'P@ssw0rd#123',
-                NODE_TLS_REJECT_UNAUTHORIZED: '0'
-            },
-            env_production: {
-                NAMESPACE: 'testing',
-                NODE_ENV: 'development',
-                PORT: 3000,
-                SECRET: 'SECRET',
-                DB_CONNECTION: 'postgres',
-                DB_HOST: '202.157.188.102',
-                DB_PORT: 5432,
-                DB_NAME: 'Testing',
-                DB_USERNAME: 'w1_user',
-                DB_PASSWORD: 'P@ssw0rd#123',
-                NODE_TLS_REJECT_UNAUTHORIZED: '0'
-            }
-        }
-    ]
+  apps: [
+    {
+      name: "Testing",
+      cwd: "/home/clouduser/backend/Testing",
+      script: "npm start",
+      exp_backoff_restart_delay: 100,
+      instances,
+      exec_mode,
+      watch,
+      max_memory_restart: "1G",
+      autorestart: true,
+      env: {
+        NAMESPACE: "Testing",
+        NODE_ENV: "development",
+        PORT: 3000,
+        SECRET: "SECRET",
+        DB_CONNECTION: "postgres",
+        DB_HOST: "202.157.188.102",
+        DB_PORT: 5432,
+        DB_NAME: "Testing",
+        DB_USERNAME: "w1_user",
+        DB_PASSWORD: "P@ssw0rd#123",
+        NODE_TLS_REJECT_UNAUTHORIZED: "0",
+      },
+      env_production: {
+        NAMESPACE: "Testing",
+        NODE_ENV: "development",
+        PORT: 3000,
+        SECRET: "SECRET",
+        DB_CONNECTION: "postgres",
+        DB_HOST: "202.157.188.102",
+        DB_PORT: 5432,
+        DB_NAME: "Testing",
+        DB_USERNAME: "w1_user",
+        DB_PASSWORD: "P@ssw0rd#123",
+        NODE_TLS_REJECT_UNAUTHORIZED: "0",
+      },
+    },
+  ],
 };
--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
